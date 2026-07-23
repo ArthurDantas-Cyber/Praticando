18dba38395cf91262b4bc7dc6cc1ab1726185cd2
Arthur Felipe Dantas Gouveia <arthurdantas.cyber@gmail.com>
2026-07-23 19:52:07 -0300

    Inclusão do exercício 1

---
+++ src/Estrutura/exc01/Main.java
@@ -0,0 +1,23 @@
+/*Escreva um programa que carregue dois valores A e B pelo teclado e imprima todos os números ímpares entre A e B.*/
+
+
+package Estrutura.exc01;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a, b;
+
+        System.out.print("Informe um número: ");
+        a = s.nextInt();
+        System.out.print("Informe outro numero, maior que o anterior: ");
+        b = s.nextInt();
+
+        for (int i = a; i < b; i++) {
+            if(i % 2 != 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
