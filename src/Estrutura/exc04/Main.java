6495847d91fe6726d427c672a331c31ff40d645c
Arthur Felipe Dantas Gouveia <arthurdantas.cyber@gmail.com>
2026-07-25 20:22:09 -0300

    add exercicio 4

---
+++ src/Estrutura/exc04/Main.java
@@ -0,0 +1,28 @@
+package Estrutura.exc04;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = 0, b = 1, c = 0, alvo, flag = 0;
+
+        System.out.print("Forneça um número para checar se o mesmo faz parte da sequência de Fibbonaci: ");
+        alvo = s.nextInt();
+
+        while (c < alvo) {
+            c = a + b;
+            if (alvo == c) {
+                flag = 1;
+                break;
+            }
+            a = b;
+            b = c;
+        }
+
+        if (flag == 1) {
+            System.out.printf("%d faz parte da sequência de fibbonaci.", alvo);
+        } else {
+            System.out.printf("%d não faz parte da sequência de fibonnaci.", alvo);
+        }
+    }
+}
