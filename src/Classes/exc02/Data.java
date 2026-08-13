9bb89b2e01058f0e8bd1bfdf9b70e02eb4431b60
Arthur Felipe Dantas Gouveia <arthurdantas.cyber@gmail.com>
2026-08-13 20:33:44 -0300

    Primeira versao exc01

--- src/Classes/exc02/Data.java
+++ src/Classes/exc02/Data.java
@@ -1,4 +1,49 @@
 package Classes.exc02;
 
 public class Data {
+    private int dia, mes, ano;
+
+    public Data (int dia, int mes, int ano){
+        //construtor
+        if(validaData(dia, mes, ano)){
+            this.dia = dia;
+            this.mes = mes;
+            this.ano = ano;
+        }else{
+            this.dia = 1;
+            this.mes = 1;
+            this.ano = 1;
+        }
+    }
+
+    private boolean validaData (int d, int m, int a){
+        boolean flag = false, vd = false, vm = false, va = false;
+        if (a > 1 && a < 9999){
+            va = true;
+        }
+        if (m >= 1 && m <= 12){
+            vm = true;
+        }
+        if (d >=1 && d <= 31){
+            vd = true;
+        }
+
+        if(va && vm & vd){
+            flag = true;
+        }
+
+        return flag;
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public int getAno() {
+        return ano;
+    }
 }
--- src/Classes/exc02/Main.java
+++ src/Classes/exc02/Main.java
@@ -1,4 +1,12 @@
 package Classes.exc02;
 
 public class Main {
+    public static void main(String[] args) {
+        Data d = new Data(21, 7, 2003);
+
+        System.out.println(d.getDia()+"/"+d.getMes()+"/"+d.getAno());
+    }
+
+
+
 }
